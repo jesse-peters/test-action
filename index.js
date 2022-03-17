6114732fe0a8c30f603bc2aa30cf7cad188d67a5
Jesse Peters <jp@jessepeters.dev>
2022-03-17 12:58:26 -0500

    more

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const getCustomDiff = require("cdk-pretty-diff");
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-
   getCustomDiff().then(() => {
     console.log(JSON.stringify(self, null, 2));
-    core.setOutput("diff", diff);
+    core.setOutput("diff", self);
   });
 
   // Get the JSON webhook payload for the event that triggered the workflow
